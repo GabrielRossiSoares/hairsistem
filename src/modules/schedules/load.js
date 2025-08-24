9af489d20df7d3aee7b59a405332b517f7c82e59
GabrielRossiSoares <gabisrossi2002@gmail.com>
2025-08-23 23:36:14 -0400

    Integração com a API

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,9 +1,17 @@
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js";
 import { hoursLoad } from "../form/hours-load.js";
+import { scheduleShow } from "./show.js";
 
 const selectedDate = document.getElementById("date");
-export function schedulesDay() {
+export async function schedulesDay() {
   // Busca na API os agendamentos para carregar do lado direito da tela.
   const date = selectedDate.value;
+
+  // Busca na API os agendamentos
+  const dailySchedules = await scheduleFetchByDay({ date });
+
+  // Exibe os agendamentos
+  scheduleShow({ dailySchedules });
   //renderiza as horas disóníveis.
   hoursLoad({ date });
   // Os horários disponíveis (horário futuro + não agendado) do lado esquerdo (form)
